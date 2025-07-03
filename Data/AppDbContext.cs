b83baf720e633a23c6655c2d15cc1c51393fd8b7
Dark14411 <carlitoseuan20010@gmail.com>
2025-07-03 10:03:13 -0600

    agregue codigo

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+    public DbSet<MonthlyBudget> MonthlyBudgets { get; set; }
+    public DbSet<BudgetCategory> BudgetCategories { get; set; }
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BudgetCategory>()
+            .HasIndex(c => new { c.Name, MonthlyBudgetId = c.MonthlyBudget.Id })
+            .IsUnique(); // prevenir nombres repetidos dentro de un mismo presupuesto
+    }
+}
